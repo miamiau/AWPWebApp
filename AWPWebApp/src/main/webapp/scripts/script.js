d97de33270af603467a3db90838e70d54b49bbdc
teodora.cosma <teodora.cosma@visma.com>
2016-01-11 18:05:22 +0200

    Routing pages... not really working

--- AWPWebApp/src/main/webapp/scripts/script.js
+++ AWPWebApp/src/main/webapp/scripts/script.js
@@ -1,12 +1,32 @@
-$(document).ready(function(){
-	/* This code is executed after the DOM has been completely loaded */
+var hotelApp = angular.module('hotelApp', [ 'ng-route' ]);
 
-	$('nav a,footer a.up').click(function(e){
+hotelApp.config(function($routeProvider) {
+	$routeProvider
 
-		// If a link has been clicked, scroll the page to the link's hash target:
+	.when('/', {
+		templateUrl : 'pages/home.html',
+		controller : 'mainController'
+	})
 
-		$.scrollTo( this.hash || 0, 1500);
-		e.preventDefault();
+	.when('/gallery', {
+		templateUrl : 'pages/gallery.html',
+		controller : 'galleryController'
+	})
+
+	.when('/contact', {
+		templateUrl : 'pages/contact.html',
+		controller : 'contactController'
 	});
+});
 
+hotelApp.controller('mainController', function($scope) {
+	$scope.message = 'Everyone come and see how good I look!';
+});
+
+hotelApp.controller('galleryController', function($scope) {
+	$scope.message = 'Look! I am an about page.';
+});
+
+hotelApp.controller('contactController', function($scope) {
+	$scope.message = 'Contact us! JK. This is just a demo.';
 });
