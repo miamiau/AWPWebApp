b5bd6102206643e8cb8c5acb902f6e4f188f14e7
teodora.cosma <teodora.cosma@visma.com>
2016-01-13 20:58:02 +0200

    Replaced with the newer version containing the routing through server

--- AWPWebApp/src/main/java/com/awp/service/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.awp.service;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("HotelApp")
-public class AppConfig extends Application{
-	
-}
--- AWPWebApp/target/AWPWebApp/scripts/script.js
+++ AWPWebApp/target/AWPWebApp/scripts/script.js
@@ -1,12 +1,44 @@
-$(document).ready(function(){
-	/* This code is executed after the DOM has been completely loaded */
+// create the module and name it hotelApp
+var hotelApp = angular.module('hotelApp', ['ngRoute']);
 
-	$('nav a,footer a.up').click(function(e){
+// configure our routes
+hotelApp.config(['$routeProvider',
+                 function($routeProvider) {
+    $routeProvider
 
-		// If a link has been clicked, scroll the page to the link's hash target:
+        // route for the home page
+        .when('/', {
+            templateUrl : 'pages/home.html',
+            controller  : 'mainController'
+        })
 
-		$.scrollTo( this.hash || 0, 1500);
-		e.preventDefault();
-	});
+        // route for the about page
+        .when('/gallery', {
+            templateUrl : 'pages/gallery.html',
+            controller  : 'galleryController'
+        })
 
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        }).
+
+        otherwise({
+          redirectTo: ''
+        });
+}]);
+
+// create the controller and inject Angular's $scope
+hotelApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+hotelApp.controller('galleryController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+hotelApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
 });
